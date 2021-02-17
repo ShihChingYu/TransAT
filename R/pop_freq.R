db0a05417253650bdbb623a34eb819228f36d05f
ShihChingYu <cyshih30@gmail.com>
2021-02-17 18:49:34 +0800

    modify

--- R/pop_freq.R
+++ R/pop_freq.R
@@ -4,6 +4,7 @@
 #'
 #' @param dat_ori a dataframe including chromosome, start and end postion, ref/alt nucleotide
 #' @param pop selection of table. Default is db_TWB_GWG_freq
+#' @importFrom magrittr `%>%`
 #' @return a new dataset with converting information
 #' @export
 #' @examples
@@ -11,20 +12,22 @@
 #'                           "anno_freq_data.csv",
 #'                           package = "MRAT"),
 #'               stringsAsFactors = FALSE, encoding = "UTF-8", row.names = NULL, sep = ",")
+#' db_file = system.file("extdata", "maf_db.db", package = "MRAT")
 #' pop_dat<-pop_freq(dat, pop)
 #'
 
-pop_freq<-function(ori_data, pop="db_gnomAD_exome_freq"){
-  colnames(ori_data)<-c("Chr", "Start", "End", "Ref", "Alt")
-  setwd("/work1782/cyshih/package/maf_db")
+pop_freq<-function(dat_ori, pop="db_gnomAD_exome_freq"){
+  colnames(dat_ori)<-c("Chr", "Start", "End", "Ref", "Alt")
+  db_file = system.file("extdata", "maf_db.db", package = "MRAT")
+  con = DBI::dbConnect(RSQLite::SQLite(), dbname = db_file)
   sqlite <- RSQLite::dbDriver("SQLite")
   conn <- RSQLite::dbConnect(RSQLite::SQLite(),"maf_db.sqlite3")
   data <- RSQLite::dbGetQuery(conn, 'SELECT * FROM db_gnomAD_exome_freq')
 
-  pop_result <- ori_data %>%
+  pop_result <- dat_ori %>%
     dplyr::left_join(data , by=c("Chr", "Start", "End", "Ref", "Alt"))
 
   return(pop_result)
-  dbDisconnect(conn)
+  RSQLite::dbDisconnect(conn)
 
 }
