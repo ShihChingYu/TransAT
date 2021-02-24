2c4137922433bb01e3fb67fedd718b3fb563d76c
ShihChingYu <cyshih30@gmail.com>
2021-02-24 16:32:55 +0800

    modify function and description

--- R/pop_freq.R
+++ R/pop_freq.R
@@ -1,33 +1,33 @@
-#' @title search allel frequency among populations and gene annotation information
+#' @title search allele frequency among populations and gene annotation information
 #' @description using genomic postion and ref/alt information to query gene annotation and allele frequency
 #' @name pop_freq
 #'
 #' @param dat_ori a dataframe including chromosome, start and end postion, ref/alt nucleotide
-#' @param pop selection of table. Default is db_TWB_GWG_freq
+#' @param pop selection of table. Default is db_TWB_NGS_freq
 #' @importFrom magrittr `%>%`
-#' @return a new dataset with converting information
+#' @return a new dataset with allele frequency and annotation information
 #' @export
 #' @examples
 #' dat<-read.csv(system.file("extdata",
 #'                           "anno_freq_data.csv",
 #'                           package = "MRAT"),
 #'               stringsAsFactors = FALSE, encoding = "UTF-8", row.names = NULL, sep = ",")
-#' db_file = system.file("extdata", "maf_db.db", package = "MRAT")
-#' pop_dat<-pop_freq(dat, pop)
+#' pop_dat<-pop_freq(dat, pop="db_TWB_NGS_freq")
 #'
 
-pop_freq<-function(dat_ori, pop="db_gnomAD_exome_freq"){
+pop_freq<-function(dat_ori, pop="db_TWB_NGS_freq"){
   colnames(dat_ori)<-c("Chr", "Start", "End", "Ref", "Alt")
-  db_file = system.file("extdata", "maf_db.db", package = "MRAT")
-  con = DBI::dbConnect(RSQLite::SQLite(), dbname = db_file)
-  sqlite <- RSQLite::dbDriver("SQLite")
-  conn <- RSQLite::dbConnect(RSQLite::SQLite(),"maf_db.sqlite3")
-  data <- RSQLite::dbGetQuery(conn, 'SELECT * FROM db_gnomAD_exome_freq')
+  con = DBI::dbConnect(RMySQL::MySQL(), user='cyshih30', password='bioinfo13579', host = "localhost", dbname="allele_freq_db")
+
+  res <- DBI::dbSendQuery(con, statement=paste("SELECT * FROM", pop))
+  data<-DBI::dbFetch(res, n = -1)
 
   pop_result <- dat_ori %>%
     dplyr::left_join(data , by=c("Chr", "Start", "End", "Ref", "Alt"))
 
   return(pop_result)
-  RSQLite::dbDisconnect(conn)
-
+  DBI::dbDisconnect(con)
 }
+
+
+
