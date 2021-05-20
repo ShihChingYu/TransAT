e1a5cba3cfd9e21eb424a41e9669faff1af613fa
ShihChingYu <cyshih30@gmail.com>
2021-05-20 15:32:27 +0800

    modify pop_freq()

--- R/pop_freq.R
+++ R/pop_freq.R
@@ -39,37 +39,40 @@ pop_freq<-function(dat_ori, pop="db_gnomAD_exome_freq"){
   pop_result <- dat_ori %>%
     dplyr::left_join(gene_table , by=c("Chr", "Start", "End", "Ref", "Alt"))
 
-  return(pop_result)
 
   #make barplot for each sub-population in the dable
-  match_col<-grep("freq$", colnames(pop_result), ignore.case = T)
-  pop_dat_forplot<-pop_result[,match_col]
+  output.pdf<-function(pop_result){
+    match_col<-grep("freq$", colnames(pop_result), ignore.case = T)
+    pop_dat_forplot<-pop_result[,match_col]
 
-  variants_maf<-list()
-  maf<-list()
-  for (i in 1:nrow(pop_dat_forplot)){
-    variants<-pop_dat_forplot[i, ]
-    for (j in 1:ncol(pop_dat_forplot)){
-      if(j %% 2 == 1){
-        maf[j]<-min(variants[,j], variants[,j+1])
+    variants_maf<-list()
+    maf<-list()
+    for (i in 1:nrow(pop_dat_forplot)){
+      variants<-pop_dat_forplot[i, ]
+      for (j in 1:ncol(pop_dat_forplot)){
+        if(j %% 2 == 1){
+          maf[j]<-min(variants[,j], variants[,j+1])
+        }
+        maf_table<-do.call("cbind", maf)
       }
-      maf_table<-do.call("cbind", maf)
+      variants_maf[[i]]<-maf_table
     }
-    variants_maf[[i]]<-maf_table
-  }
-  var_maf_table<-do.call("rbind", variants_maf)
-  colnames(var_maf_table)<-unlist(unique(strsplit(colnames(pop_dat_forplot), "_([^_]*_[^_]*)$")))
+    var_maf_table<-do.call("rbind", variants_maf)
+    colnames(var_maf_table)<-unlist(unique(strsplit(colnames(pop_dat_forplot), "_([^_]*_[^_]*)$")))
 
-  pdf(file = paste(getwd(), "plots.pdf", sep="/"), onefile = TRUE)
-  for(i in 1:nrow(var_maf_table)){
-    dat<-var_maf_table[i, ]
-    dat2<-data.frame(dat)
-    ggpl<-ggplot2::ggplot(dat2, aes(x=rownames(dat2), y=dat, color=rownames(dat2))) +
-      ggplot2::geom_bar(stat = "identity", fill="white") +xlab('Population') +
-      ylab('Frequency') + coord_flip() + theme(legend.position = "none")
-    print(ggpl)}
-  dev.off()
+    pdf(file = paste(tempdir(), "plots.pdf", sep="\\"), onefile = TRUE)
+    for(i in 1:nrow(var_maf_table)){
+      dat<-var_maf_table[i, ]
+      dat2<-data.frame(dat)
+      ggpl<-ggplot2::ggplot(dat2, aes(x=rownames(dat2), y=dat, color=rownames(dat2))) +
+        ggplot2::geom_bar(stat = "identity", fill="white") +xlab('Population') +
+        ylab('Frequency') + coord_flip() + theme(legend.position = "none")
+      print(ggpl)}
+    grDevices::dev.off()
+  }
 
   DBI::dbDisconnect(con)
+  output.pdf(pop_result)
+  return(pop_result)
 }
 
